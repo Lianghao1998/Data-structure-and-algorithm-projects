adc6627f3ddd6c6de7de4ed586190e27c0c3bd6b
Lianghao1998 <117904573+Lianghao1998@users.noreply.github.com>
2022-12-15 00:05:02 +0800

    Deque interface uploaded

---
+++ Deque.java
@@ -0,0 +1,20 @@
+package deque;
+
+public interface Deque<T> {
+    public void addFirst(T item);
+    public void addLast(T item);
+    public default boolean isEmpty(){
+        if(size() == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    public int size();
+    public void printDeque();
+
+    public T removeFirst();
+    public T removeLast();
+    public T get(int index);
+}
